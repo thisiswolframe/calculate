1b76ce740f89fc6ecbe180043b36b968ff8d26a6
thisiswolframe <148793034+thisiswolframe@users.noreply.github.com>
2024-09-27 22:54:41 +0600

    Add files via upload

--- all-content.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('header ul.centered');
-
-    menuToggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
